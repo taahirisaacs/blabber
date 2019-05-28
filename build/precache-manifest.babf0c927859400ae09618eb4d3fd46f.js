a9d8a54bd1acfff216f78574b3ab34bfb99495c8
Taahir <15251376+taahirisaacs@users.noreply.github.com>
2019-05-28 22:30:32 +0200

    Test

---
+++ build/precache-manifest.babf0c927859400ae09618eb4d3fd46f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a10a8c43a39ac5b0511d2ec300512f30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a4c2253877b47da0669",
+    "url": "/static/css/main.20c4a1c7.chunk.css"
+  },
+  {
+    "revision": "8a33cd6eb00e387660cc",
+    "url": "/static/js/2.c2b57aa5.chunk.js"
+  },
+  {
+    "revision": "8a4c2253877b47da0669",
+    "url": "/static/js/main.9ce90092.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
